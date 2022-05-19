358260fc4a082357e604d39eb658d8f1b541daec
daniel <drk98@nau.edu>
2022-05-18 23:11:23 -0700

    Added relative

--- phase_curve_tools/__init__.py
+++ phase_curve_tools/__init__.py
@@ -4,6 +4,6 @@ __author__ = """Daniel Kramer"""
 __email__ = 'drk98@nau.edu'
 __version__ = '0.1.0'
 
-from hg import HG as bowellHG
-from reducedMag import calcReducedMag
-from useAstroQuery import HorAbsMags
+from .hg import HG as bowellCalcAbsMag
+from .reducedMag import calcReducedMag
+from .useAstroQuery import HorAbsMags
