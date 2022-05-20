07a2c692549a73def739e3e77f19ef9db2b49d2c
daniel <drk98@nau.edu>
2022-05-20 10:17:01 -0700

    updated

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,10 @@ import phase_curve_tools
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.bibtex']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.bibtex', 'sphinx.ext.mathjax']
 bibtex_bibfiles = ['refs.bib']
+mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
