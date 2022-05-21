e5cb9487bffc9543c788561be23f542cfa638778
daniel <drk98@nau.edu>
2022-05-20 20:15:17 -0700

    added documentation

--- phase_curve_tools/reducedMag.pyx
+++ phase_curve_tools/reducedMag.pyx
@@ -13,4 +13,18 @@ cdef _calcReducedMag(valid_arg mag, valid_arg helioDist, valid_arg obsDist):
     return mag - 5*np.log10(helioDist*obsDist)
 
 def calcReducedMag(valid_arg mag, valid_arg helioDist, valid_arg obsDist):
+    r"""Calculate teh reduced magnitude
+    ..math:
+
+        V(\alpha) = m - 5\log10(\Delta r)
+    
+    :param mag: The magnitudes of the observations
+    :type mag: np.ndarray
+    :param helioDist: The heliocentric distances
+    :type helioDist: np.ndarray
+    :param obsDist: The observer/Earth distances
+    :type obsDist: np.ndarray
+    :return: The reduced magnitudes
+    :rtype: np.ndarray
+    """
     return _calcReducedMag(mag, helioDist, obsDist)
