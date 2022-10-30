9f7cc2398d70ef2c042bd75f55d3404ebad06c3a
Daniel Kramer <drk98@gmail.com>
2022-10-30 00:14:43 -0700

    Added stub file

---
+++ phase_curve_tools/__init__.pyi
@@ -0,0 +1,6 @@
+from .bestFit import BestFit as BestFit, HGResult as HGResult
+from .hg import bowellCalcAbsMag as bowellCalcAbsMag, calcPhaseCurve as calcPhaseCurve
+from .hg12 import calcHG12 as calcHG12
+from .hg1g2 import calcHG1G2 as calcHG1G2
+from .reducedMag import calcReducedMag as calcReducedMag
+from .useAstroQuery import HorAbsMags as HorAbsMags
